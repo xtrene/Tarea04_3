c4f7b296a4337031d2263932dab5f9bf60fe612a
Carlos <carlos@xtrene.com>
2021-06-21 00:11:59 +0200

    Agregar archivos de proyecto.

---
+++ Tarea04_3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Tarea04_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce tu nombre: ");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Introduce tu edad: ");
+            string edad = Console.ReadLine();
+
+            Console.WriteLine("Te llamas {0} y tienes {1} años.", nombre, edad);
+
+            Console.ReadKey();
+        }
+    }
+}
